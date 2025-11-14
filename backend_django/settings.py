e1082bdba621d0134b05e3f667ea129285f4c036
Emix235 <135489251+Emix235@users.noreply.github.com>
2025-11-14 00:55:12 -0600

    Quitar un dato inutil
    Replaced the environment variable for GENERATIVE_AI_KEY with a hardcoded value.

--- backend_django/settings.py
+++ backend_django/settings.py
@@ -22,8 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# GENERATIVE_AI_KEY = os.getenv('GENERATIVE_AI_KEY')
-GENERATIVE_AI_KEY = "AIzaSyBIMd3ew67RNOsQJ4EmNJk_Hw8nB4jL4VQ"
 # if not GENERATIVE_AI_KEY:
 #     raise ValueError('GENERATIVE_AI_KEY environment variable not set')
 
